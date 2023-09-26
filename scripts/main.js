57ed993f650914ba5e9697cd710f803b4106800d
Sithum2002 <sithumasam@gmail.com>
2023-09-26 13:02:11 +0530

    background image and historical weather API

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "06faa0f569dc4db6ada123522231809"; 
+const API_KEY = "06faa0f569dc4db6ada123522231809";
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
         return `<div class="details">
@@ -96,5 +96,117 @@ const getUserCoordinates = () => {
 locationButton.addEventListener("click", getUserCoordinates);
 searchButton.addEventListener("click", getCityCoordinates);
 cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
+
+
+
+
+
+// Initialize the map
+const map = L.map('map').setView([0, 0], 13); // Default view at coordinates [0, 0], zoom level 13
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Function to display a location on the map
+function displayLocationOnMap(latitude, longitude) {
+    map.setView([latitude, longitude], 13); // Set the view to the specified coordinates and zoom level
+    L.marker([latitude, longitude]).addTo(map); // Add a marker at the location
+}
+
+// Event listener for the "Search" button
+document.getElementById('search-button').addEventListener('click', () => {
+    const locationInput = document.getElementById('location-input').value.trim();
+    if (locationInput === '') return;
+
+    // You can use a geocoding service to get coordinates for the entered location
+    // For simplicity, let's assume a function getCoordinatesFromLocation that returns coordinates
+    const { latitude, longitude } = getCoordinatesFromLocation(locationInput);
+
+    // Display the location on the map
+    displayLocationOnMap(latitude, longitude);
+});
+
+// Function to get coordinates from a location (You can implement this function using a geocoding service)
+function getCoordinatesFromLocation(location) {
+    // This is a placeholder function, you should use a geocoding service like Nominatim or Mapbox Geocoding API
+    // to convert the location input to latitude and longitude.
+    // For example, you can make an API request to a geocoding service and parse the response.
+    // Replace this with your actual implementation.
+    return {
+        latitude: 0, // Replace with actual latitude
+        longitude: 0, // Replace with actual longitude
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to create historical weather cards
+const createHistoricalWeatherCard = (weatherItem) => {
+    return `
+      <div class="historical-card">
+        <h3>${weatherItem.date}</h3>
+        <img src="${weatherItem.day.condition.icon}" alt="weather-icon">
+        <h6>Temp: ${weatherItem.day.maxtemp_c}°C</h6>
+        <h6>Wind: ${weatherItem.day.maxwind_kph} KPH</h6>
+        <h6>Humidity: ${weatherItem.day.avghumidity}%</h6>
+      </div>
+    `;
+  };
+
+  
+  // Function to fetch historical weather data
+  const getHistoricalWeather = (cityName) => {
+    const today = new Date();
+    const endDate = today.toISOString().slice(0, 10); // End date is today
+    today.setDate(today.getDate() - 5); // Start date is 5 days ago
+    const startDate = today.toISOString().slice(0, 10);
+  
+    const HISTORICAL_API_URL = `https://api.weatherapi.com/v1/history.json?key=${API_KEY}&q=${cityName}&dt=${startDate}&end_dt=${endDate}`;
+  
+    fetch(HISTORICAL_API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        const historicalData = data.forecast.forecastday;
+  
+        // Clear previous historical weather data
+        const historicalWeatherDiv = document.querySelector(".historical-weather");
+        historicalWeatherDiv.innerHTML = "";
+  
+        // Create and add historical weather cards to the DOM
+        historicalData.forEach((weatherItem) => {
+          const html = createHistoricalWeatherCard(weatherItem);
+          historicalWeatherDiv.insertAdjacentHTML("beforeend", html);
+        });
+      })
+      .catch(() => {
+        alert("An error occurred while fetching historical weather data!");
+      });
+  };
+  
+  // Add an event listener to the historical weather button
+  const historicalWeatherButton = document.querySelector(".historical-weather-btn");
+  historicalWeatherButton.addEventListener("click", () => {
+    const cityName = cityInput.value.trim();
+    if (cityName === "") return;
+    getHistoricalWeather(cityName);
+  });
+  
+
 
 
